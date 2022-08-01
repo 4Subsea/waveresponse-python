cf1ca03154063bbf5bb5b02f7866149ce5c27d10
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2022-08-01 17:31:10 +0200

    tests Grid

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,71 @@
+import pytest
+import numpy as np
 from scarlet_lithium import Grid
 
 
 class Test_Grid:
-    def test_something(self):
-        assert 1 == 1
+    def test__init__(self):
+        freq = np.linspace(0, 1.0, 10)
+        dirs = np.linspace(0, 360.0, 15, endpoint=False)
+        vals = np.zeros((10, 15))
+        grid = Grid(freq, dirs, vals, freq_hz=True, degrees=True, clockwise=True, waves_coming_from=True)
+
+        np.testing.assert_array_almost_equal(grid._freq, 2.0 * np.pi * freq)
+        np.testing.assert_array_almost_equal(grid._dirs, (np.pi / 180.0) * dirs)
+        np.testing.assert_array_almost_equal(grid._vals, vals)
+        assert grid._clockwise is True
+        assert grid._waves_coming_from is True
+
+    def test__init__2(self):
+        freq = np.linspace(0, 1.0, 10)
+        dirs = np.linspace(0, 2.0 * np.pi, 15, endpoint=False)
+        vals = np.zeros((10, 15))
+        grid = Grid(freq, dirs, vals, freq_hz=False, degrees=False, clockwise=False, waves_coming_from=False)
+
+        np.testing.assert_array_almost_equal(grid._freq, freq)
+        np.testing.assert_array_almost_equal(grid._dirs, dirs)
+        np.testing.assert_array_almost_equal(grid._vals, vals)
+        assert grid._clockwise is False
+        assert grid._waves_coming_from is False
+
+    def test__init__raises_duplicate_freq(self):
+        with pytest.raises(ValueError):
+            freq = np.array([0, 1, 1, 2])
+            dirs = np.array([0, 1, 2])
+            vals = np.zeros((4, 3))
+            Grid(freq, dirs, vals)
+
+    def test__init__raises_duplicate_dirs(self):
+        with pytest.raises(ValueError):
+            freq = np.array([0, 1, 2])
+            dirs = np.array([0, 1, 1, 2])
+            vals = np.zeros((3, 4))
+            Grid(freq, dirs, vals)
+
+    def test__init__raises_negative_freq(self):
+        with pytest.raises(ValueError):
+            freq = np.array([-1, 1, 2, 3])
+            dirs = np.array([0, 1, 2])
+            vals = np.zeros((4, 3))
+            Grid(freq, dirs, vals)
+
+    def test__init__raises_negative_dirs(self):
+        with pytest.raises(ValueError):
+            freq = np.array([0, 1, 2, 3])
+            dirs = np.array([-1, 1, 2])
+            vals = np.zeros((4, 3))
+            Grid(freq, dirs, vals)
+
+    def test__init__raises_freq_not_increasing(self):
+        with pytest.raises(ValueError):
+            freq = np.array([0, 1, 2, 1])
+            dirs = np.array([0, 1, 2])
+            vals = np.zeros((4, 3))
+            Grid(freq, dirs, vals)
+
+    def test__init__raises_dirs_not_increasing(self):
+        with pytest.raises(ValueError):
+            freq = np.array([0, 1, 2])
+            dirs = np.array([0, 1, 2, 1])
+            vals = np.zeros((3, 4))
+            Grid(freq, dirs, vals)
