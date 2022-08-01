fd8f88344eb2707df8092f646404e153832713d5
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2022-08-01 16:57:39 +0200

    add one test

---
+++ tests/test_core.py
@@ -0,0 +1,6 @@
+from scarlet_lithium import Grid
+
+
+class Test_Grid:
+    def test_something(self):
+        assert 1 == 1
--- tests/test_something.py
+++
@@ -1,5 +0,0 @@
-import scarlet_lithium
-
-
-def test_something():
-    assert 1 == 1
