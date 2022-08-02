2b4a77c5ae2490d21f4eb9e3e7ce9c6e26cc2d19
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2022-08-02 14:34:37 +0200

    update init

--- src/scarlet_lithium/__init__.py
+++ src/scarlet_lithium/__init__.py
@@ -1,5 +1,9 @@
-from ._core import Grid
+from ._core import Grid, complex_to_polar, polar_to_complex
 
 __version__ = "0.0.1"
 
-__all__ = ["Grid"]
+__all__ = [
+    "complex_to_polar",
+    "Grid",
+    "polar_to_complex",
+]
