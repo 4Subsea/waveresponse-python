cf9d80f8d1d85e95a74451c07639407778067134
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2022-08-11 14:46:52 +0200

    add calculate_response to init

--- src/scarlet_lithium/__init__.py
+++ src/scarlet_lithium/__init__.py
@@ -5,11 +5,13 @@ from ._core import (
     WaveSpectrum,
     complex_to_polar,
     polar_to_complex,
+    calculate_response,
 )
 
 __version__ = "0.0.1"
 
 __all__ = [
+    "calculate_response",
     "complex_to_polar",
     "DirectionalSpectrum",
     "Grid",
