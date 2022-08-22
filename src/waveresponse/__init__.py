dab56ea3f65562d9d8ecff63097d74e7fdc9ea40
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2022-08-22 19:29:19 +0200

    black + isort

--- src/waveresponse/__init__.py
+++ src/waveresponse/__init__.py
@@ -7,7 +7,12 @@ from ._core import (
     complex_to_polar,
     polar_to_complex,
 )
-from ._transform import rigid_transform, rigid_transform_surge, rigid_transform_sway, rigid_transform_heave
+from ._transform import (
+    rigid_transform,
+    rigid_transform_heave,
+    rigid_transform_surge,
+    rigid_transform_sway,
+)
 
 __version__ = "0.0.1"
 
--- src/waveresponse/_transform.py
+++ src/waveresponse/_transform.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from ._core import RAO
 
 
