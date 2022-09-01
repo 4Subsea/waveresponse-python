84c0226da61ba3456ee5b8c2efdd3787e2cf3129
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2022-09-01 20:13:03 +0200

    add test

--- src/waveresponse/_core.py
+++ src/waveresponse/_core.py
@@ -448,8 +448,8 @@ class Grid:
         array :
             Interpolated grid values.
         """
-        freq = np.asarray_chkfinite(freq).copy()
-        dirs = np.asarray_chkfinite(dirs).copy()
+        freq = np.asarray_chkfinite(freq).reshape(-1).copy()
+        dirs = np.asarray_chkfinite(dirs).reshape(-1).copy()
 
         if freq_hz:
             freq = 2.0 * np.pi * freq
