d76e95f344272144b0ad9c5a097c8fab8fd228ae
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2022-09-23 13:07:21 +0200

    refactor with BaseWave1d

--- src/waveresponse/_standardized1d.py
+++ src/waveresponse/_standardized1d.py
@@ -1,7 +1,72 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 
 
-class BasePMSpectrum:
+class BaseWave1d(ABC):
+    """
+    Base class for handling and generating 1-D wave spectra.
+    """
+
+    def __init__(self, freq, freq_hz=False):
+        self._freq = np.asarray_chkfinite(freq).copy()
+        self._freq_hz = freq_hz
+
+        if self._freq_hz:
+            self._freq *= 2.0 * np.pi
+
+    def __call__(self, *args, freq_hz=None, **kwargs):
+        """
+        Generate wave spectrum.
+
+        Parameters
+        ----------
+        *args
+            Spectrum input arguments.
+        freq_hz : bool, optional
+            Whether to return the frequencies and spectrum in terms of rad/s (`True`)
+            or Hz (`False`). If `None` (default), the original units of `freq` is
+            preserved.
+        **kwargs
+            Spectrum input keyword arguments.
+
+        Return
+        ------
+        freq : 1-D array
+            Frequencies corresponding to the spectrum values. Unit is set according
+            to `freq_hz`.
+        spectrum : 1-D array
+            Spectrum values. Unit is set according to `freq_hz`.
+
+        Notes
+        -----
+        The scaling between wave spectrum in terms of Hz and rad/s is defind
+        as:
+
+        ``S(f) = 2*pi*S(w)``
+
+        where ``S(f)`` and ``S(w)`` are the same spectrum but expressed
+        in terms of Hz and rad/s, respectively.
+        """
+        freq = self._freq.copy()
+        spectrum = self._spectrum(freq, *args, **kwargs)
+
+        if freq_hz is None:
+            freq_hz = self._freq_hz
+
+        if freq_hz:
+            scale = 2.0 * np.pi
+        else:
+            scale = 1.0
+
+        return freq / scale, spectrum * scale
+
+    @abstractmethod
+    def _spectrum(omega, *args, **kwargs):
+        raise NotImplementedError()
+
+
+class BasePMSpectrum(BaseWave1d):
     """
     Base class for handling 1-D Pierson-Moskowtiz (PM) type spectra of the form:
 
@@ -14,13 +79,6 @@ class BasePMSpectrum:
     freq_hz : bool
         Whether the provided frequencies are in rad/s (default) or Hz.
     """
-
-    def __init__(self, freq, freq_hz=False):
-        self._freq = np.asarray_chkfinite(freq).copy()
-        self._freq_hz = freq_hz
-
-        if self._freq_hz:
-            self._freq *= 2.0 * np.pi
 
     def __call__(self, A, B, freq_hz=None):
         """
@@ -55,18 +113,7 @@ class BasePMSpectrum:
         where ``S(f)`` and ``S(w)`` are the same spectrum but expressed
         in terms of Hz and rad/s, respectively.
         """
-        freq = self._freq.copy()
-        spectrum = self._spectrum(freq, A, B)
-
-        if freq_hz is None:
-            freq_hz = self._freq_hz
-
-        if freq_hz:
-            scale = 2.0 * np.pi
-        else:
-            scale = 1.0
-
-        return freq / scale, spectrum * scale
+        return super().__call__(A, B, freq_hz=freq_hz)
 
     def _spectrum(self, omega, A, B):
         return A / omega**5.0 * np.exp(-B / omega**4)
@@ -237,3 +284,52 @@ class JONSWAP(ModifiedPiersonMoskowitz):
         sigma[arg] = sigma_a
         sigma[~arg] = sigma_b
         return sigma
+
+
+# class OchiHubble(ModifiedPiersonMoskowitz):
+#     """
+#     Ochi-Hubble wave spectrum (derived from modified Pierson-Moskowitz), given as:
+
+#     ``S(w) = A/w**5 exp(-B/w**4)``
+
+#     where,
+
+#     - ``A = ((4 * q + 1) / 4 * w_p**4)**q * Hs**2 / (4 * gamma(q) * w**(4 * (q - 1)))``
+#     - ``B = (4 * q + 1) / 4 * w_p**4``.
+
+#     and ``q`` is a user-defined shape parameter. Note that ``gamma`` is the "Gamma function".
+
+#     Parameters
+#     ----------
+#     freq : array-like
+#         Sequence of frequencies to use when generating the spectrum.
+#     freq_hz : bool
+#         Whether the provided frequencies are in rad/s (default) or Hz.
+#     q : float or callable
+#         A scalar value or a callable that accepts hs and tp, and returns
+#         a scalar value.
+
+#     Notes
+#     -----
+#     The special case ``q=1`` corresponds to the modified Pierson-Moskowitz spectrum.
+#     """
+
+#     def _A(self, *args):
+#         """
+#         Spectrum parameter.
+#         """
+#         hs, tp = args
+#         q = self._q(*args)
+#         omega_p = 2.0 * np.pi / tp
+#         a = ((4.0 * q + 1.0) / 4.0 * omega_p**4) ** q
+#         A = (a * hs**2) / (4 * gammafun(q) * self._freq ** (4 * (q - 1.0)))
+#         return A
+
+#     def _B(self, *args):
+#         """
+#         Spectrum parameter.
+#         """
+#         _, tp = args
+#         q = self._q(*args)
+#         omega_p = 2.0 * np.pi / tp
+#         return (4 * q + 1) / 4 * omega_p**4
