ea1788d72431a409442fad6c55d85fbac6a1e6e4
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-01-11 14:39:40 +0100

    enh: extend mirror function to take symmetry plane

--- src/waveresponse/_core.py
+++ src/waveresponse/_core.py
@@ -167,7 +167,36 @@ def _cast_to_grid(grid):
     return new
 
 
-def mirror(rao, dof):
+def _check_mirror_xz(dirs, degrees=False):
+    dirs = np.asarray_chkfinite(dirs).copy()
+
+    if degrees:
+        dirs = dirs * np.pi / 180.0
+
+    sin_dirs = np.sin(dirs)
+    # sin_dirs = sin_dirs[sin_dirs != 0.0]   # exclude zeros for robustness
+
+    if not (sin_dirs >= 0.0).all() or not (sin_dirs >= 0.0).all():
+        raise ValueError(
+            "`rao` must be defined in the range [0, 180] degrees or [180, 360) degrees."
+        )
+
+
+def _check_mirror_yz(dirs, degrees=False):
+    pass
+
+
+def _sort(dirs, vals):
+    """
+    Sort directions and values according to (unsorted) directions.
+    """
+    dirs = np.asarray_chkfinite(dirs)
+    vals = np.asarray_chkfinite(vals)
+    sorted_args = np.argsort(dirs)
+    return dirs[sorted_args], vals[:, sorted_args]
+
+
+def mirror(rao, dof, sym_plane="xz"):
     """
     Mirrors an RAO object with symmetry in x-z plane, defined in the directional range
     [0, 180) degrees (or [0, numpy.pi) radians).
@@ -197,22 +226,33 @@ def mirror(rao, dof):
             "`dof` must be 'surge', 'sway', 'heave', 'roll', 'pitch' or 'yaw'"
         )
 
-    if (dirs < 0.0).any() or (dirs > periodicity / 2.0).any():
-        raise ValueError(
-            "`rao` must be defined between 0 and 180 degrees (or 0 and 2pi radians)."
-        )
+    if sym_plane.lower() == "xz":
+        change_switch = -1
+        bounds = (0.0, periodicity / 2.0)
+        _check_mirror_xz(dirs, degrees=rao._degrees)
+    elif sym_plane.lower() == "yz":
+        change_switch = 1
+        bounds = (periodicity / 4.0, 3.0 * periodicity / 4.0)
+        _check_mirror_yz(dirs, degrees=rao._degrees)
 
-    if dof.lower() in ("sway", "roll", "yaw"):
+    if sym_plane.lower() == "xz" and dof.lower() in ("sway", "roll", "yaw"):
+        scale_phase = -1
+    elif sym_plane.lower() == "yz" and dof.lower() in ("surge", "pitch", "yaw"):
         scale_phase = -1
     else:
         scale_phase = 1
 
-    mirror_mask = (dirs != 0) & (dirs != periodicity / 2)
+    exclude_bounds_mask = (dirs != bounds[0]) & (dirs != bounds[1])
 
     vals_mirrored = np.concatenate(
-        (vals, scale_phase * vals[:, mirror_mask][:, ::-1]), axis=1
+        (vals, scale_phase * vals[:, exclude_bounds_mask][:, ::-1]), axis=1
     )
-    dirs_mirrored = np.concatenate((dirs, periodicity - dirs[mirror_mask][::-1]))
+    dirs_mirrored = np.concatenate(
+        (dirs, change_switch * dirs[exclude_bounds_mask][::-1] + periodicity)
+    )
+    dirs_mirrored = _robust_modulus(dirs_mirrored, periodicity)
+
+    dirs_mirrored, vals_mirrored = _sort(dirs_mirrored, vals_mirrored)
 
     return RAO(
         freq,
