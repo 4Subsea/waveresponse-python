bug: fix dirm degrees default and wrong scaling when .reshape is called (#85)

src/waveresponse/_core.py
@@ -1818,12 +1818,6 @@ class DirectionalSpectrum(_SpectrumMixin, Grid):
 
         vals_new = interp_fun(freq_new, dirs_new)
 
-        if freq_hz:
-            vals_new *= 2.0 * np.pi
-
-        if degrees:
-            vals_new *= np.pi / 180.0
-
         new = self.copy()
         new._freq, new._dirs, new._vals = freq_new, dirs_new, vals_new
         return new
@@ -2003,9 +1997,6 @@ class DirectionalBinSpectrum(_SpectrumMixin, Grid):
 
         vals_new = interp_fun(freq_new, self._dirs)
 
-        if freq_hz:
-            vals_new *= 2.0 * np.pi
-
         new = self.copy()
         new._freq, new._vals = freq_new, vals_new
         return new
@@ -2148,6 +2139,9 @@ class WaveSpectrum(DisableComplexMixin, DirectionalSpectrum):
             the direction is returned in 'radians'. Defaults to original unit used
             during instantiation.
         """
+
+        if degrees is None:
+            degrees = self._degrees
 
         dp, sp = self.spectrum1d(axis=0, degrees=False)
 
@@ -2296,6 +2290,9 @@ class WaveBinSpectrum(DisableComplexMixin, DirectionalBinSpectrum):
             the direction is returned in 'radians'. Defaults to original unit used
             during instantiation.
         """
+
+        if degrees is None:
+            degrees = self._degrees
 
         dirm = self._mean_direction(*self.spectrum1d(axis=0, degrees=False))
 